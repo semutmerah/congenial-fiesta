924b6062850f71026040f1d84b083bb133bcc0a0
semutmerah <rasyid@rasyidfahroni.com>
2022-11-09 15:14:39 +0700

    test again post build

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,12 +1,5 @@
-if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
-    HOCKEYAPP_API_TOKEN={API_Token}
-    HOCKEYAPP_APP_ID={APP_ID}
+#!/usr/bin/env bash
 
-    # Example: Upload main branch app binary to HockeyApp using the API
-    if [ "$APPCENTER_BRANCH" == "master" ];
-     then
-        echo "Ini jalanin post-build"
-    else
-        echo "Current branch is $APPCENTER_BRANCH"
-    fi
+if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
+    echo "Ini jalanin post-build"
 fi
